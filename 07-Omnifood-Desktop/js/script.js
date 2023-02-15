2ec2c68d87b07d33de0677e20932b8b03dd0884f
PlatinuPL <platont@interia.pl>
2023-02-15 17:48:32 +0100

    Fixing flexbox to support safari

--- 07-Omnifood-Desktop/js/script.js
+++ 07-Omnifood-Desktop/js/script.js
@@ -52,13 +52,19 @@ const sectionHeroEl = document.querySelector(".section-hero");
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
-    if (ent.isIntersecting === false) console.log(ent);
-    document.body.classList.add("sticky");
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
   },
   {
     //  In the viewport
     root: null,
     threshold: 0,
+    rootMargin: "-80px",
   }
 );
 obs.observe(sectionHeroEl);
